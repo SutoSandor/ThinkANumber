602eee4b2096ec2243e800400e328185f8c2726b
SutoSandor <sandorsuto97@gmail.com>
2018-10-19 13:07:56 +0200

    Befejezett alkalmazás
    Elméleten befejeztem a cuccot.

--- app/src/main/java/e/soola/thinkanumber/MainActivity.java
+++ app/src/main/java/e/soola/thinkanumber/MainActivity.java
@@ -1,6 +1,8 @@
 package e.soola.thinkanumber;
 
 import android.content.Context;
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,6 +16,10 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     private ImageView kep1;
+    private ImageView kep2;
+    private ImageView kep3;
+    private ImageView kep4;
+    private ImageView kep5;
     private TextView szam;
     private Button gomb_plusz;
     private Button gomb_minusz;
@@ -21,17 +27,32 @@ public class MainActivity extends AppCompatActivity {
     private int x = 0;
     private Random rand = new Random();
     private int random = rand.nextInt(10);
-
+    private int eletero = 5;
+    private Button gomb_ujrakezd;
+    private void ujrakezd(){
+        kep1.setImageResource(R.drawable.heart2);
+        kep2.setImageResource(R.drawable.heart2);
+        kep3.setImageResource(R.drawable.heart2);
+        kep4.setImageResource(R.drawable.heart2);
+        kep5.setImageResource(R.drawable.heart2);
+        random = rand.nextInt(10);
+        gomb_kuldes.setEnabled(true);
+        eletero = 5;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        gomb_ujrakezd = (Button) findViewById(R.id.gomb_ujrakezd);
         szam = (TextView) findViewById(R.id.szam);
         gomb_plusz = (Button) findViewById(R.id.gomb_plusz);
         gomb_minusz = (Button) findViewById(R.id.gomb_minusz);
         gomb_kuldes = (Button) findViewById(R.id.gomb_kuldes);
         kep1 = (ImageView) findViewById(R.id.sziv_1);
+        kep2 = (ImageView) findViewById(R.id.sziv_2);
+        kep3 = (ImageView) findViewById(R.id.sziv_3);
+        kep4 = (ImageView) findViewById(R.id.sziv_4);
+        kep5 = (ImageView) findViewById(R.id.sziv_5);
         gomb_plusz.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -73,7 +94,6 @@ public class MainActivity extends AppCompatActivity {
                     Context context = getApplicationContext();
                     CharSequence text = "NYERTÉL!";
                     int duration = Toast.LENGTH_LONG;
-
                     Toast toast = Toast.makeText(context, text, duration);
                     toast.show();
                     gomb_kuldes.setEnabled(false);
@@ -81,23 +101,38 @@ public class MainActivity extends AppCompatActivity {
                 }
                 if(x>random){
                     Context context = getApplicationContext();
-                    kep1.setImageResource(R.drawable.heart1);
+
                     CharSequence text = "Kevesebb!";
                     int duration = Toast.LENGTH_SHORT;
-
+                    eletero--;
                     Toast toast = Toast.makeText(context, text, duration);
                     toast.show();
                 }
                 if(x<random){
                     Context context = getApplicationContext();
-                    kep1.setImageResource(R.drawable.heart1);
+
                     CharSequence text = "Több!";
                     int duration = Toast.LENGTH_SHORT;
-
+                    eletero--;
                     Toast toast = Toast.makeText(context, text, duration);
                     toast.show();
                 }
+                switch (eletero){
+                    case 4: kep1.setImageResource(R.drawable.heart1); break;
+                    case 3: kep2.setImageResource(R.drawable.heart1); break;
+                    case 2: kep3.setImageResource(R.drawable.heart1); break;
+                    case 1: kep4.setImageResource(R.drawable.heart1); break;
+                    case 0: kep5.setImageResource(R.drawable.heart1);
+                            gomb_kuldes.setEnabled(false);break;
+                }
             }
         });
+        gomb_ujrakezd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ujrakezd();
+            }
+        });
+
     }
 }
